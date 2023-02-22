c40de6899d4d328ca6603d17b76aa14b2a4841a6
TheExecuter1 <siddiqui.fuzail1930@gmail.com>
2023-02-22 22:42:23 +0530

    minor

---
+++ Array/revision.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class revision {
+
+    static class Pair {
+        int min;
+        int max;
+    };
+    
+
+    
+    static Pair mexEle(int arr[], int n) {
+        Pair minmx = new Pair();
+        if (n == 1){
+            minmx.min = arr[0];
+            minmx.max = arr[0];
+            return minmx;
+        }
+
+        if(arr[0]>arr[1]){
+            minmx.max = arr[0];
+            minmx.min = arr[1];
+        }
+        else{
+            minmx.max = arr[1];
+            minmx.min = arr[0];
+        }
+
+        for(int i=2 ; i<n; i++){
+            if (arr[i] > minmx.max) {
+                minmx.max = arr[i];
+            } else if (arr[i] < minmx.min) {
+                minmx.min = arr[i];
+            }
+        }
+
+        return minmx;
+    }
+
+
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int arr[] = new int[n];
+        for (int i=0 ; i<n ; i++){
+            arr[i]=s.nextInt();
+        }
+        
+        Pair minmx=  mexEle(arr,n);
+        System.out.println("min" + minmx.min);
+        System.out.println("max" + minmx.max);
+
+    }
+}
